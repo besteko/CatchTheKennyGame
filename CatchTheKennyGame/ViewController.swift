9e42fecaceeaace5c54d0e2cbb91fa81cbd61a41
Beste Kocaoğlu <78082138+besteko@users.noreply.github.com>
2023-02-07 19:29:38 +0300

    Initial Commit

---
+++ CatchTheKennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by Beste Kocaoglu on 7.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
